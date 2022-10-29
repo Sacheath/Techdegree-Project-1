4d5d8c6d8017f8a9e87cb1d0c203400d3a0df9af
Sacheath "Sonny" Son <cheathson@gmail.com>
2022-10-29 14:05:53 -0400

    Ready for review
    Going for exceeds expectations.

--- guessing_game.py
+++ guessing_game.py
@@ -1,77 +1,60 @@
 import random
 from statistics import mode, median, mean
 
+high_score = []
+
 def start_game():
     print("Welcome to Team Treehouse's first project: Number Guessing Game!")
-    
-    answer = random.randrange(1,100)
-    actual_guesses = []
-    high_score = []  
-    
-    
-    
-    while True:
-        try:
-            confirm_game = input("Are you sure you want to give it a shot? Y/N: > ")
-            if confirm_game.lower() == 'n':
-                print("Sorry to hear that, have a nice day!")
-                exit()
-            if confirm_game.lower() == 'y':
-                if len(high_score) < 1:
-                    print("There is no current high score!")
-                else:
-                    print("The current high score is {} attempts!".format(min(high_score)))
-                print("Pick a number between 1 and 100!")
-                break
-            else:
-                raise ValueError()
-        except ValueError:
-            print("That's not a valid selection. Please enter either Y or N.")
-            continue
-    
-    while True:
-        try:
-            player_guess = int(input("> "))
-            if player_guess <= 0 or player_guess > 100:
-                raise ValueError()
-        except ValueError:
-            print("That's not a valid guess. Please only use whole numbers between 1 and 100.")
-            continue
-            
-        if player_guess > answer:
-            print("It's lower!")
-            actual_guesses.append(player_guess)
-            continue
-        elif player_guess < answer:
-            print("It's higher!")
-            actual_guesses.append(player_guess)
-            continue
-        else:
-            print("You got it right in only {} attempts!".format(len(actual_guesses)))
-            high_score.append(len(actual_guesses))
-            break
-    
-    while True:
-        try:
-            restart = input ("How about another round? Y/N: > ")
-            if restart.lower() == 'y':                
-                print("Coming right up!")
-                #how do I restart the game?
-            if restart.lower() == 'n':
-                print("""
-        Sorry to hear that. Here's your stats for this round!
-    
-        Attempts: {}
-        Mean Guess: {}
-        Median Guess: {}
-        Mode Guess: {}
-    
-        Come back later if you ever want to try again!
-                """.format(total_guesses, round(mean(actual_guesses)), round(median(actual_guesses)), round(mode(actual_guesses))))
-                exit()
-            else:
-                raise ValueError()
-        except ValueError:
-            print("Sorry that's not a valid selection. Please enter either Y or N.")
+    print ("Pick a number between 1 and 100!")
 
+    while True:
+        total_guesses = 0
+        answer = random.randrange(1,100)
+
+        while True:
+            try:
+                player_guess = int(input("> "))
+                if player_guess <= 0 or player_guess > 100:
+                    raise ValueError()
+            except ValueError:
+                print("That's not a valid guess. Please only use whole numbers between 1 and 100.")
+                continue
+            if player_guess > answer:
+                print("It's lower!")
+                total_guesses += 1
+                continue
+            elif player_guess < answer:
+                print("It's higher!")
+                total_guesses += 1
+                continue
+            else:
+                print(f"You got it right in only {total_guesses} attempts!")
+                high_score.append(total_guesses)
+                break
+
+        while True:
+            try:
+                confirm_game = input("Do you want to play again?  > ")
+                    
+                if confirm_game.lower() == 'n':
+                    print(f"""
+                    Sorry to hear that. Here's your stats for this round!
+
+                    Attempts: {total_guesses}
+                    Mean Guess: {mean(high_score):.0f}
+                    Median Guess: {median(high_score)}
+                    Mode Guess: {mode(high_score)}
+
+                    Come back later if you ever want to try again!""")
+                    exit()
+
+                elif confirm_game.lower() == 'y':
+                    print(f"\nThe current high score is {min(high_score)} attempts!\n")
+                    answer = random.randrange(1,100)
+                    start_game()
+                else:
+                    raise ValueError()
+            except ValueError:
+                print("That's not a valid selection. Please enter either Y or N.")
+                continue
 start_game()
